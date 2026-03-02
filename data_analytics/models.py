5e99d6b0718c319693052113165376ebd81b7973
Sourav Das <souravdas007123@gmail.com>
2026-03-02 11:17:48 +0530

    update

--- data_analytics/models.py
+++ data_analytics/models.py
@@ -4,5 +4,5 @@ from django.db import models
 class Data(models.Model):
     first_name=models.CharField(max_length=100)
     last_name=models.CharField(max_length=100)
-    age=models.IntegerField()
-    salary=models.IntegerField()
+    age=models.PositiveIntegerField(max_length=2)
+    salary=models.PositiveIntegerField()
